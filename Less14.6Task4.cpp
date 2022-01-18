0b1646422693ec9a59a685aa65359b16d4de4fa5
Владимир Тетерин <havingworld@gmail.com>
2022-01-18 13:23:10 +1000

    Добавьте файлы проекта.

---
+++ Less14.6Task4.cpp
@@ -0,0 +1,36 @@
+﻿/*
+Требуется реализовать небольшую программу для сравнения двух двумерных матриц размерами 4х4 на предмет их полного равенства. 
+Программа принимает на вход две целочисленные матрицы A и B, которые вводятся с помощью std::cin и сравнивает их на предмет 
+полного равенства. Если матрицы равны, то об этом сообщается в стандартном выводе. Алгоритм должен быть по возможности оптимальным 
+и не проводить лишних операций.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arrov1[4][4]{}, arrov2[4][4]{};
+    bool bArrovIdentical = true;
+    cout << "input first arrow: ";
+    for (int n = 0; n < 4; n++) {
+        for (int m = 0; m < 4; m++) {
+            cin >> arrov1[n][m];
+        }
+    }
+
+    cout << "input second arrow: ";
+    for (int n = 0; n < 4; n++) {
+        for (int m = 0; m < 4; m++) {
+            cin >> arrov2[n][m];
+            if (arrov1[n][m] != arrov2[n][m]) {
+                bArrovIdentical = false;
+                break;
+            }
+        }
+        if (!bArrovIdentical) break;
+    }
+
+    (bArrovIdentical) ? cout << "arrows identical" : cout << "arrows don`t identical";
+}
+
